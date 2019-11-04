5b7bae5e9bf2723961924267ae2903308d02b5b5
mfvaldez <mfvaldez.projects@hotmail.com>
2019-11-04 19:16:40 -0300

    routes

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-app.get("/", (req, res) => res.send("HELLO WORLD"));
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
 
 // Connect to MongoDB
-mongoose.connect('mongodb://DiegoM11:ogeid019@myapp-lhs7m.gcp.mongodb.net/test?retryWrites=true&w=majority').then( res => {
+mongoose.connect('mongodb+srv://DiegoM11:ogeid019@myapp-lhs7m.gcp.mongodb.net/Myapp?retryWrites=true&w=majority').then( res => {
   console.log("Connected to DB")}).catch( err => {
     console.log("err: " + err);
   });
-/*const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://DiegoM11:<password>@myapp-lhs7m.gcp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});*/
 
+  app.get("/", (req, res) => res.send("HOME"));
+  app.get("/test", (req, res) => res.send('HELLO WORLD'));
 
+  app.listen(port, () => console.log(`Server running on port ${port}`));
