5a8f771da9c08b15e61a31fb9d69e2888ea8cba0
mfvaldez <mfvaldez.projects@hotmail.com>
2019-11-04 21:26:30 -0300

    ultimo

--- models/City.js
+++ models/City.js
@@ -3,6 +3,12 @@ const Schema = mongoose.Schema;
 
 let CitiesSchema = new Schema({
     name: String,
-    country: String
-  })
+    country: String,
+    tags: { type: [String], index: true }
+  });
+
+CitiesSchema.index({name: 1, country: -1 });
+
 module.exports = mongoose.model('Cities', CitiesSchema);
+
+mongoose.connect('mongodb+srv://DiegoM11:ogeid019@myapp-lhs7m.gcp.mongodb.net/Myapp?retryWrites=true&w=majority', { autoIndex: false });
