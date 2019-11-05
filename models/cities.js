da55bdab5d36cb0ad794886ac1d815f2c1783483
Diego <massaradiego11@gmail.com>
2019-11-04 23:25:24 -0300

    ultimo

--- models/cities.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-let CitiesSchema = new Schema({
-    name: String,
-    country: String
-  })
-module.exports = User = mongoose.model('Cities', CitiesSchema);
