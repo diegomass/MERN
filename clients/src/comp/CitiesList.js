284362a7d3288ce9ea5131ca6d3e5ab77dfb1c9a
mfvaldez <mfvaldez.projects@hotmail.com>
2019-11-06 20:56:04 -0300

    last

--- clients/src/comp/CitiesList.js
+++ clients/src/comp/CitiesList.js
@@ -2,21 +2,44 @@ import React from 'react';
 
 const CITIES_SERVICE_URL = 'localhost:5000/cities';
 
-        const CitiesList = ({cities}) =>
-        cities.map(cities => <li key={cities}>{cities}</li>)
+class CitiesList extends React.Component{
 
-        componentDidMount(); {
+    constructor(props){
+        super();
+        this.state = {
+          isFetching: false,
+          cities: null
+        }
+    }
+        // class CitiesList = ({cities}) =>
+        // cities.map(cities => <li key={cities}>{cities}</li>)
+
+        componentDidMount() {
             this.fetchCities()
           }
           
           fetchCities = () => {
-            this.setState({...this.state, isFetching: true})
+            this.setState({...this.state, isFetching: true});
             fetch(CITIES_SERVICE_URL)
               .then(response => response.json())
-              .then(result => this.setState({cities: result, isFetching: false}))
+              .then(result => {
+                  this.setState({cities: result, isFetching: false});
+                  console.log(this.state.cities);
+                } )
               .catch(e => console.log(e));
           }
 
+render(){
+    return <div> {class CitiesList { cities } =
 
+        cities.map(cities => <li key={cities}>{cities}</li>)}
+    }
+     </div>
+
+
+}
+
+
+}
 
 export default CitiesList;
