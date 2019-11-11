71f1f8d3ff418fd9346eb4fc70ff26b5ee4c9a12
mfvaldez <mfvaldez.projects@hotmail.com>
2019-11-11 19:46:33 -0300

    redux

--- clients/src/actions/citiesActions.js
+++ clients/src/actions/citiesActions.js
@@ -1,7 +1,8 @@
 import { GET_CITIES, ADD_CITIES, DELETE_CITIES } from './types';
 
-export const getCities = () => {
+export const getCities = (payload) => {
     return {
-        type: GET_CITIES
+        type: GET_CITIES,
+        payload: payload
     };
 }
